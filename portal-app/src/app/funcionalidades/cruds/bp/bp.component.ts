a12e53606643e55ba2a5031940b9719633f01f39
João Pedro Pinheiro <jprnp18@gmail.com>
2018-12-02 16:03:43 -0200

    Continuação mov

--- portal-app/src/app/funcionalidades/cruds/bp/bp.component.ts
+++ portal-app/src/app/funcionalidades/cruds/bp/bp.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any;
 import { Bem } from '../../../models/bem.model';
 import { BpService } from './bp.service';
 import { SalaService } from '../sala/sala.service';
@@ -19,12 +19,25 @@ export class BpComponent implements OnInit {
   bemUpdate: Bem = new Bem();
   findOneById: any;
   salas: Sala[];
+  admin: boolean;
+  dateHtml: any;
+  date: any;
 
   constructor(private router: Router, private bemService: BpService, private salaService: SalaService) {
 
   }
 
   ngOnInit() {
+
+    if (localStorage.length > 0) {
+      let user = JSON.parse(localStorage.getItem('user'));
+      console.log("DENTRO DE BP: " + user.departamento.nome);
+      if (user.departamento.nome == "Departamento de Patrimônio") {
+        this.admin = true;
+      } else {
+        this.admin = false;
+      }
+    }
     this.bemService.getBens()
       .subscribe(data => {
         this.bens = data;
@@ -36,13 +49,18 @@ export class BpComponent implements OnInit {
   };
 
   findOne(bem: Bem): void {
+
+    this.dateHtml = new Date().toLocaleDateString('pt-BR');
+    console.log();
     this.bemService.findOne(bem)
       .subscribe(data => {
         this.findOneById = data;
+        this.bemUpdate = this.findOneById;
       })
   };
 
   deleteBem(bem: Bem): void {
+
     this.bemService.deleteBem(bem)
       .subscribe(data => {
         this.bens = this.bens.filter(u => u !== bem);
@@ -50,7 +68,7 @@ export class BpComponent implements OnInit {
   };
 
   updateBem(): void {
-    this.bemUpdate.id = this.findOneById.id;
+
     console.log(this.bemUpdate);
     this.bemService.updateBem(this.bemUpdate)
       .subscribe(data => {
@@ -60,24 +78,27 @@ export class BpComponent implements OnInit {
   };
 
   baixarBem(): void {
-    this.bemUpdate.id = this.findOneById.id;
-    this.bemUpdate.denominacao = this.findOneById.denominacao;
-    this.bemUpdate.especificacao = this.findOneById.especificacao;
-    this.bemUpdate.numNotaFiscal = this.findOneById.numNotaFiscal;
-    this.bemUpdate.marca = this.findOneById.marca;
-    this.bemUpdate.garantia = this.findOneById.garantia;
-    this.bemUpdate.valorCompra = this.findOneById.valorCompra;
-    this.bemUpdate.valorAtual = this.findOneById.valorAtual;
-    this.bemUpdate.vidaUtil = this.findOneById.vidaUtil;
-    this.bemUpdate.dataAquis = this.findOneById.dataAquis;
-    this.bemUpdate.numTombamento = this.findOneById.numTombamento;
-    this.bemUpdate.grupoMaterial = this.findOneById.grupoMaterial;
-    console.log(this.bemUpdate);
+
+    if (this.bemUpdate.motivoDaBaixa == null) {
+      alert("Informe o motivo da baixa.");
+      return;
+    }
+    let tomorrow = new Date();
+    let today = new Date();
+    tomorrow.setDate(today.getDate() + 1);
+    this.date = tomorrow.toLocaleDateString('fr-CA');
+
+    this.bemUpdate.dataDaBaixa = this.date;
+
     this.bemService.baixarBem(this.bemUpdate)
       .subscribe(data => {
         alert('Bem baixado!');
         location.reload();
       })
   };
+
+  getAdmin(): boolean {
+    return this.admin;
+  }
 
 }
